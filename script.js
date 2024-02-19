efacf2a6fa18b5b2cc644b7e4fb7fa9831d1b0bd
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-19 22:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 
-       let inp = document.querySelectorAll("input")
-    let btn = document.querySelector("button")
-
-    btn.addEventListener("click", form)
-
-    function form(eventDetails)
-    {
-        let firstInp = inp[0].value;
-        let lastInp = inp[1].value;
-
-        alert(firstInp +" "+ lastInp)
-    }
 
 
 
